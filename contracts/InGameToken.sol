5e864918e5034d475922d42724853bd123b9ebd9
chaoukis <chaoukisammy@gmail.com>
2021-10-25 10:16:10 -0400

    Remove unecessary smart contracts

--- contracts/InGameToken.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity >=0.4.22 <0.9.0;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract GLDToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("HelloWorld", "HWL") {
-        _mint(msg.sender, initialSupply);
-    }
-}
